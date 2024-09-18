dbb8864056b7d0e163d889f5ad28e8cbc835db12
psinfinity <priyanshsaha.ps@gmail.com>
2024-09-18 23:47:36 +0530

    q41, q42

---
+++ q026-q050/q041.py
@@ -0,0 +1,6 @@
+# https://www.hackerrank.com/challenges/polar-coordinates/problem
+
+import cmath
+z=input()
+print(cmath.polar(complex(z))[0])
+print(cmath.polar(complex(z))[1])
---
+++ q026-q050/q042.py
@@ -0,0 +1,8 @@
+# https://www.hackerrank.com/challenges/find-angle/problem
+
+import math
+
+ab = int(input())
+bc = int(input())
+
+print(str(int(math.degrees(math.atan(bc/ab))))+'\u00b0')
